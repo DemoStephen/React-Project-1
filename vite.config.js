88cb5a8186bb9e2ffa97d0e5dfe82e5b33c8baa7
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-08-22 13:18:55 +0100

    fix: some bug

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/https://github.com/DemoStephen/React-Project-1/",
+  base: "/react-project-1/",
 });
