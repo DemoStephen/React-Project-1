1d7e915d2b6e4e7659a1cc14170a6ca0fb3095a7
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-08-24 23:15:48 +0100

    add: added util file. This file contains information about the recipe page

---
+++ src/util.js
@@ -0,0 +1,47 @@
+export const recipeInfo = {
+  ingredient: [
+    "2-3 large eggs",
+    "Salt, to taste",
+    "Pepper, to taste",
+    "1 tablespoon of butter or oil",
+    "Optional fillings: cheese, diced vegetables, cooked meats, herbs",
+  ],
+  instruction: [
+    {
+      title: "Beat the eggs:",
+      content:
+        "In a bowl, beat the eggs with a pinch of salt and pepper until they are well mixed. You can add a tablespoon of water or milk for a fluffier texture.",
+    },
+    {
+      title: "Heat the pan:",
+      content:
+        "Place a non-stick frying pan over medium heat and add butter or oil.",
+    },
+    {
+      title: "Cook the omelette:",
+      content:
+        "Once the butter is melted and bubbling, pour in the eggs. Tilt the pan to ensure the eggs evenly coat the surface.",
+    },
+    {
+      title: "Add fillings (optional):",
+      content:
+        "When the eggs begin to set at the edges but are still slightly runny in the middle, sprinkle your chosen fillings over one half of the omelette.",
+    },
+    {
+      title: "Fold and serve:",
+      content:
+        "As the omelette continues to cook, carefully lift one edge and fold it over the fillings. Let it cook for another minute, then slide it onto a plate.",
+    },
+  ],
+  tableRow: [
+    { tableHead: "Calories", tableData: "277kcal" },
+    { tableHead: "Carbs", tableData: "0g" },
+    { tableHead: "Protein", tableData: "20g" },
+    { tableHead: "Fat", tableData: "22g" },
+  ],
+  preparation: [
+    { name: "Total", text: "Approximately 10 minutes" },
+    { name: "Preparation", text: "5 minutes" },
+    { name: "Cooking", text: "5 minutes" },
+  ],
+};
